764f7d549c077fc304ff7bfbad7587b98c58dbc5
Mark Fang <microspaze@qq.com>
2025-07-11 17:31:11 +0800

    Fix errors.

---
+++ PodIJKMedia.h
@@ -0,0 +1,13 @@
+//
+//  PodIJKMedia.h
+//  Pods
+//
+//  Created by 畅三江 on 2022/7/12.
+//
+
+#ifndef PodIJKMedia_h
+#define PodIJKMedia_h
+
+#import <IJKMediaFramework/IJKMediaFramework.h>
+
+#endif /* PodIJKMedia_h */
